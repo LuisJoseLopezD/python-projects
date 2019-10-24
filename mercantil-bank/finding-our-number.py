d12be6b0ef3567c4b1b3df57c60af6a689be1b8f
LuisJoseLopezD <45377927+LuisJoseLopezD@users.noreply.github.com>
2019-10-24 01:59:55 -0400

    Update finding-our-number.py

--- mercantil-bank/finding-our-number.py
+++ mercantil-bank/finding-our-number.py
@@ -3,21 +3,21 @@
 ##cedula = []
 ##
 ##for i in range(8):
-##    numeros = int(input("Ingrese su cedula: "))
+##    numeros = int(input("Isert your cedula: "))
 ##    cedula.append(numeros)
 ##
-##print("Su numero son los 3 ultimos digitos de su cedula: ")
+##print("Your numbers are the last 3 ones from your cedula ")
 ##print(cedula[5],cedula[6],cedula[7])
 
-##cedula = input("Ingrese su cedula: ")
+##cedula = input("Insert your cedula: ")
 ##list(cedula)
 ##
-##print("Su numero son los 3 ultimos digitos de su cedula: ")
+##print("Your numbers are the last 3 ones from your cedula: ")
 ##print(cedula[5],cedula[6],cedula[7])
 
 i=0;
-cedula = input("Ingrese su cedula: ")
+cedula = input("Insert your cedula: ")
 list(cedula)
 
-print("Su numero son los 3 ultimos digitos de su cedula: ")
+print("Your numbers are the last 3 ones from your cedula: ")
 print(cedula[i-3],cedula[i-2],cedula[i-1])
